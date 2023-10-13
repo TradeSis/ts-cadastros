040cd22479ca6a2bdb2727c9240976c9291a791b
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2023-10-13 14:51:15 -0300

    adicionado classe para tab de configurações

--- configuracao/index.php
+++ configuracao/index.php
@@ -20,7 +20,7 @@ include_once(__DIR__ . '/../head.php');
   
 </style>
 <div class="container-fluid">
-  <div class="row">
+  <div class="row mt-2">
     <div class="col-md-2 mb-3">
       <ul class="nav nav-pills flex-column" id="myTab" role="tablist">
         <?php
@@ -31,7 +31,7 @@ include_once(__DIR__ . '/../head.php');
         //echo "<HR>stab=" . $stab;
         ?>
         <li class="nav-item ">
-          <a class="nav-link <?php if ($stab == "usuario") {
+          <a class="nav-link ts-tabConfig <?php if ($stab == "usuario") {
             echo " active ";
           } ?>"
             href="?tab=configuracao&stab=usuario" role="tab">Usuários</a>
