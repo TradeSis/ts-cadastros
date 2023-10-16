ab2818cef5d283783023c01f4434425f755a6055
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2023-10-16 11:27:29 -0300

    removido style das tabs de configurações

--- index.php
+++ index.php
@@ -43,32 +43,32 @@ $nivelMenu = $nivelMenuLogin['nivelMenu'];
                                 $tab = 'clientes';
                             } ?>
                             <li class="nav-item mr-1">
-                                <a class="nav-link1 nav-link 
+                                <a class="nav-link 
                                 <?php if ($tab == "clientes") {echo " active ";} ?>" 
                                 href="?tab=clientes" role="tab">Clientes</a>
                             </li>
                         <?php }
                         if ($nivelMenu >= 2) { ?>
                             <li class="nav-item mr-1">
-                                <a class="nav-link1 nav-link <?php if ($tab == "produtos") {echo " active ";} ?>"
+                                <a class="nav-link <?php if ($tab == "produtos") {echo " active ";} ?>"
                                 href="?tab=produtos" role="tab">Produtos</a>
                             </li>
                         <?php }
                         if ($nivelMenu >= 2) { ?>
                             <li class="nav-item mr-1">
-                                <a class="nav-link1 nav-link <?php if ($tab == "marcas") {echo " active ";} ?>" 
+                                <a class="nav-link <?php if ($tab == "marcas") {echo " active ";} ?>" 
                                 href="?tab=marcas" role="tab">Marcas</a>
                             </li>
                         <?php }
                         if ($nivelMenu >= 2) { ?>
                             <li class="nav-item mr-1">
-                                <a class="nav-link1 nav-link <?php if ($tab == "servicos") {echo " active ";} ?>" 
+                                <a class="nav-link <?php if ($tab == "servicos") {echo " active ";} ?>" 
                                 href="?tab=servicos" role="tab">Serviços</a>
                             </li>
                         <?php }
                         if ($nivelMenu >= 4) { ?>
                             <li class="nav-item mr-1">
-                                <a class="nav-link1 nav-link <?php if ($tab == "configuracao") {echo " active ";} ?>" 
+                                <a class="nav-link <?php if ($tab == "configuracao") {echo " active ";} ?>" 
                                 href="?tab=configuracao" role="tab" data-toggle="tooltip" data-placement="top" title="Configurações"><i class="bi bi-gear"></i> Configurações</a>
                             </li>
                         <?php } ?>
